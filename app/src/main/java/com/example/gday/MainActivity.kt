a48c600636a248a7adb9d69f05fc10556e034dbc
Jocheu <jotomaniak@gmail.com>
2022-03-30 08:28:39 +0200

    Teraz poprawnie

--- app/src/main/java/com/example/gday/MainActivity.kt
+++ app/src/main/java/com/example/gday/MainActivity.kt
@@ -2,12 +2,7 @@ package com.example.gday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.Toast
-import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
 import com.example.gday.databinding.ActivityMainBinding
 
 
@@ -18,45 +13,44 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(R.layout.activity_main)
 
-        setChipGroupDynamically()
+        setupChipGroup()
     }
 
-    private fun setChipGroupDynamically(){
-        val chipGroup = ChipGroup(this)
-        chipGroup.layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        chipGroup.isSingleSelection = true
-        chipGroup.isSingleLine = false
-
-        chipGroup.addView(createChip("India"))
-        chipGroup.addView(createChip("Nepal"))
-        chipGroup.addView((createChip("USA")))
-        chipGroup.addView(createChip("South Africa"))
-
-        binding.rootContainer.addView(chipGroup)
-    }
-
-    private fun createChip(label:String):Chip{
-        val chip = Chip(this, null, R.style.Widget_MaterialComponents_Chip_Entry)
-        chip.layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-
-        chip.text = label
-        chip.isCloseIconVisible = true
-        chip.isCheckable = true
-        chip.isClickable = true
-        chip.setOnCloseIconClickListener {
-            Toast.makeText(this, "Chip deleted succesfully!", Toast.LENGTH_SHORT).show()
+    private fun setupChipGroup() {
+        binding.chip1.setOnCloseIconClickListener {
+            binding.chipGroup.removeView(it)
+            Toast
+                .makeText(this, "Removed 1st Chip", Toast.LENGTH_SHORT)
+                .show()
         }
-        return chip
+
+        binding.chip2.setOnCloseIconClickListener {
+            binding.chipGroup.removeView(it)
+            Toast
+                .makeText(this, "Removed 2nd Chip", Toast.LENGTH_SHORT)
+                .show()
+        }
+        binding.chip3.setOnCloseIconClickListener {
+            binding.chipGroup.removeView(it)
+            Toast
+                .makeText(this, "Removed 3rd Chip", Toast.LENGTH_SHORT)
+                .show()
+        }
+        binding.chip4.setOnCloseIconClickListener {
+            binding.chipGroup.removeView(it)
+            Toast
+                .makeText(this, "Removed 4th Chip", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
+
+
+
+
 
 }
